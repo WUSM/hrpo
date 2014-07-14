fcaa9857da326ae242f578f711e6c0e78d724364
Sam Hermes <hermessam@mpa-l-hermes.wucon.wustl.edu>
2014-07-14 10:24:32 -0500

    Remove unnecessary code from front page

--- front-page.php
+++ front-page.php
@@ -1,30 +1,5 @@
-<?php
-	get_header();
+<?php get_header(); ?>
 
-	if ( have_posts() ) {
-		while (have_posts()) :
-			the_post();
-			$class = '';
-			if ( get_the_post_thumbnail() != '' ) {
-				$class = ' notch';
-				echo '<div id="featured-image">';
-				the_post_thumbnail();
-				echo '</div>';
-			}
-?>
-
-<div id="main" class="clearfix">
-
-	<div class="wrapper">
-
-		<?php get_sidebar( 'left' ); ?>
-		
-		<article class="front-page<?php echo $class; ?>">
-	<?php
-			the_title('<h1>', '</h1>');
-			the_content();
-		endwhile;
-	} else { ?>
 <div id="main" class="clearfix">
 
 		<?php get_sidebar( 'left' ); ?>
@@ -33,7 +8,7 @@
 				<div class="mission clearfix">
 					<div class="mission-left">
 						<h1>Mission of HRPO<br>and the IRB</h1>
-						<p>Our mission is to protect the rights and welfare of part- icipants in “human research” as defined in 45 CFR 46.102(d) and (f) and “clinical inves- tigations” as defined in 21 CFR 50.3(c).</p>
+						<p>Our mission is to protect the rights and welfare of participants in “human research” as defined in 45 CFR 46.102(d) and (f) and “clinical investigations” as defined in 21 CFR 50.3(c).</p>
 					</div>
 					<div class="mission-right">
 						<img src="wp-content/themes/hrpo/_/img/accreditation.png">
@@ -105,7 +80,7 @@
 	                        printf('<li class="calendar clearfix"><div class="calendar-date"><div class="calendar-month">%s</div><div class="calendar-day">%s</div></div><div class="event-info-single"><a href="%s"><div class="calendar-title">%s</div></a><div class="time">Time: %s - %s</div></div</li>',
 	                            eo_get_the_start('M', $event->ID, null, $event->occurrence_id),
 	                            eo_get_the_start('j', $event->ID, null, $event->occurrence_id),
-	                             get_permalink($event->ID),
+	                            get_permalink($event->ID),
 	                            get_the_title($event->ID),
 	                            eo_get_the_start('g:ia', $event->ID, null, $event->occurrence_id),
 	                            eo_get_the_end('g:ia', $event->ID, null, $event->occurrence_id)
@@ -114,9 +89,6 @@
 	                    echo '</ul>';
 	                } else { } ?>
 			</div>
-
-
-	<?php } ?>
 
 	</div>
 
